232a668ef1d6fd5608da3dcbf3cee32472b66991
r614 <steradian614@gmail.com>
2019-07-02 16:03:41 -0700

    Fixed global levelling.

--- construct.py
+++ construct.py
@@ -7,25 +7,30 @@ class Construct:
     playerList = {}
     diff = 5
     tier1 = ['Magikarp', 'Poliwag', 'Goldeen', 'Tentacool', 'Staryu', 'Kingler', 'Slowpoke', 'Krabby', 'Horsea','Gyrados']
-    tier2 =['Rapidash', 'Xatu', 'Pidgey', 'Wurmple', 'Starly', 'Hoothoot', 'Pineco']
+    tier2 =['Rapidash', 'Xatu', 'Pidgey', 'Wurmple', 'Starly', 'Hoothoot', 'Pineco' ]
 
+    worldChain = Blockchain()
 
     def updateTier(player):
         if(player.getLevel() > 20 ):
             second = tier1
             second.append(tier2)
-            self.playerList[player][0] = second
+            self.playerList[player] = second
 
     def join(self, player):
-        worldChain = Blockchain()
-        list = [tier1, worldChain]
-        playerList[player] = list
+        playerList[player] = tier1
 
     def travel(self, player):
-        worldChain = playerList[player][1]
         data = {
             'name': str(random.choice(playerList[player])),
-            'level': random.randint(player.getLevel - self.adiff + 2, player.getLevel + self.diff)
+            'level': random.randint(player.getLevel - self.diff + 2, player.getLevel + self.diff)
         }
-        player.receivePokemon(worldChain.mine(Block(data, player.getLevel())))
+        player.receivePokemon(worldChain.mine(Block(data, getDiff())
         updateTier(player)
+
+    def getDiff(self):
+        avglevel = 0
+        for players in self.playerList.keys():
+            avglevel += players.getLevel()
+        avglevel = avglevel/len(self.playerList.keys())
+        return avglevel + self.diff
