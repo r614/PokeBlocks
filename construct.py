427f8d3b5e72129c10d70e0ee0cd7946e1260d0d
r614 <steradian614@gmail.com>
2019-07-02 15:57:47 -0700

    Added individual blockchains for each player.

--- construct.py
+++ construct.py
@@ -4,23 +4,25 @@ from player import Player
 import random
 
 class Construct:
-    playerList = []
+    playerList = {}
     diff = 5
     tier1 = ['Magikarp', 'Poliwag', 'Goldeen', 'Tentacool', 'Staryu', 'Kingler', 'Slowpoke', 'Krabby', 'Horsea','Gyrados']
     tier2 =['Rapidash', 'Xatu', 'Pidgey', 'Wurmple', 'Starly', 'Hoothoot', 'Pineco']
 
-    worldChain = Blockchain()
 
     def updateTier(player):
         if(player.getLevel() > 20 ):
             second = tier1
             second.append(tier2)
-            self.playerList[player] = second
+            self.playerList[player][0] = second
 
     def join(self, player):
-        playerList.append(player, tier1)
+        worldChain = Blockchain()
+        list = [tier1, worldChain]
+        playerList[player] = list
 
     def travel(self, player):
+        worldChain = playerList[player][1]
         data = {
             'name': str(random.choice(playerList[player])),
             'level': random.randint(player.getLevel - self.adiff + 2, player.getLevel + self.diff)
